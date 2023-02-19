be1fe286f1b456668b9e2fbb3a0fe4c9f1651ebc
Changyu Shin <scv1702@naver.com>
2023-02-19 15:17:58 +0900

    establish: initialize spring boot project

---
+++ src/test/java/kr/ac/knu/gdsc/Eywa/EywaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.knu.gdsc.Eywa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EywaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
