86b7abc7784960edb72965df6a6e0ea024865a88
HyeonJunSong <music9801@naver.com>
2023-03-11 15:14:28 +0900

    feat: AI model import and classify
    tflite model, label import and classify
    used tflite package

---
+++ lib/model/service/alien_species_descriminate.dart
@@ -0,0 +1,24 @@
+
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+
+void loadModel() {
+  Tflite.loadModel(
+    model: "assets/models/model.tflite",
+    labels: "assets/models/labels.txt",
+  );
+}
+
+void classifyImage(XFile image) async {
+  var output = await Tflite.runModelOnImage(
+      path: File(image!.path).path,
+      imageMean: 0.0, // defaults to 117.0
+      imageStd: 255.0, // defaults to 1.0
+      numResults: 2, // defaults to 5
+      threshold: 0.2, // defaults to 0.1
+      asynch: true // defaults to true
+  );
+  print(output);
+}
