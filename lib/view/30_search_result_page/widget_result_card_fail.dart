6e8dde0a6fb8e0d1335fe35c5021c7f1b927562c
HyeonJunSong <music9801@naver.com>
2023-03-11 15:18:55 +0900

    feat: photo search page UI featured
    search result page and result card fail or success

---
+++ lib/view/30_search_result_page/widget_result_card_fail.dart
@@ -0,0 +1,62 @@
+import 'package:eywa_client/view_model/search_page_view_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:cross_file_image/cross_file_image.dart';
+
+Widget resultCardFail(BuildContext context) => Positioned(
+  top: 125.h,
+  child: Container(
+    width: 300.w,
+    height: 600.h,
+    decoration: BoxDecoration(
+      color: context.theme.primaryColor,
+      borderRadius: BorderRadius.circular(20.r),
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        _image(),
+        _text(context),
+      ],
+    ),
+  ),
+);
+
+Widget _image() => Container(
+  width: 300.w,
+  height: 300.w,
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(20.r),
+    image: DecorationImage(
+      image: XFileImage(Get.find<SearchPageViewController>().image!),
+      fit: BoxFit.cover,
+    ),
+  ),
+);
+
+Widget _text(BuildContext context) => Container(
+  width: 300.w,
+  height: 539.h - 300.w,
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text(
+        "This is not",
+        style: TextStyle(
+          color: context.theme.backgroundColor,
+          fontSize: 30.sp,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      Text(
+        "a alien species",
+        style: TextStyle(
+          color: context.theme.backgroundColor,
+          fontSize: 30.sp,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    ],
+  ),
+);
