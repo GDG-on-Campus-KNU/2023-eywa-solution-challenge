5c81cf90c1ae3969c3839f43e3db6182b4aae886
Changyu Shin <scv1702@naver.com>
2023-03-22 02:34:32 +0900

    feat: implement oauth2.0 redirect router

--- src/main/java/kr/ac/knu/gdsc/Eywa/members/controller/MembersController.java
+++ src/main/java/kr/ac/knu/gdsc/Eywa/members/controller/MembersController.java
@@ -3,17 +3,17 @@ package kr.ac.knu.gdsc.Eywa.members.controller;
 import kr.ac.knu.gdsc.Eywa.members.domain.Member;
 import kr.ac.knu.gdsc.Eywa.members.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/members")
 @RequiredArgsConstructor
+@RequestMapping(value = "/members")
 public class MembersController {
     private final MemberService memberService;
 
@@ -21,5 +21,21 @@ public class MembersController {
     public Member getMember(@AuthenticationPrincipal OAuth2User oAuth2User) {
         Optional<Member> memberOptional = this.memberService.getMember(oAuth2User);
         return memberOptional.orElse(null);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/session")
+    public ResponseEntity<?> getSession(HttpServletRequest request, @CookieValue("JSESSIONID") String sessionId) {
+        String userAgent = request.getHeader("user-agent");
+        boolean isMobile = userAgent.matches(".*(iPhone|iPod|iPad|BlackBerry|Android|Windows CE|LG|MOT|SAMSUNG|SonyEricsson|IOS_APP|ANDROID_APP).*");
+        HttpHeaders headers = new HttpHeaders();
+        if (isMobile) {
+            String appLink = "com.googleusercontent.apps.1060976925872-qp92sbr1qjm0peso9ttqa40dgkjpff5t";
+            String redirect = appLink + "://?JSESSIONID=" + sessionId;
+            headers.add("Location", redirect);
+        } else {
+            String redirect = "/";
+            headers.add("Location", redirect);
+        }
+        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).headers(headers).build();
     }
 }
