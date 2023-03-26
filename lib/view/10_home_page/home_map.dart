f8d37105b67522b5bab904ec7fb39f29fe2f1be4
HyeonJunSong <music9801@naver.com>
2023-03-27 01:08:03 +0900

    feat: Marker implementation
    custom marker implementation

--- lib/view/10_home_page/home_map.dart
+++ lib/view/10_home_page/home_map.dart
@@ -1,24 +1,99 @@
+import 'package:custom_map_markers/custom_map_markers.dart';
+import 'package:eywa_client/model/field_guid_element.dart';
+import 'package:eywa_client/view/50_detail_page/detail_animal_page.dart';
+import 'package:eywa_client/view/50_detail_page/detail_plant_page.dart';
+import 'package:eywa_client/view_model/field_guide_page_controller.dart';
 import 'package:eywa_client/view_model/home_page_controller.dart';
 import 'package:eywa_client/view_model/user_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:get/get.dart';
 
-Widget homeMap() {
+Widget homeMap(BuildContext context) {
   return Positioned(
-    child: GoogleMap(
-      zoomControlsEnabled: false,
-      onMapCreated: Get.find<HomePageController>().onMapCreated,
-      initialCameraPosition: CameraPosition(
-        target: Get.find<UserController>().curPosition.value,
-        zoom: 15,
-      ),
-      markers: Set<Marker>.from(Get.find<HomePageController>().markers.map(
-        (e) => Marker(
-          markerId: MarkerId(e.id.toString()),
-          position: e.coor
-        )
-      ),
+    child: Obx(() => CustomGoogleMapMarkerBuilder(
+      customMarkers: List<MarkerData>.from(Get.find<HomePageController>().reports.map((e) {
+        return MarkerData(
+          marker: Marker(
+            markerId: MarkerId(e.reportId.toString()),
+            position: e.registerInfo.coor,
+            onTap: (){
+              bool ifPlant = false;
+              Get.find<FieldGuidePageController>().plantElements.forEach((element) {
+                if(element.id == e.registerInfo.dictionaryId){
+                  ifPlant = true;
+                }
+              });
+
+              if(ifPlant){
+                FieldGuideElementPlant element = Get.find<FieldGuidePageController>().plantElements.firstWhere((element) => element.id == e.registerInfo.dictionaryId);
+                Get.to(DetailPlant(element: element, imagePath: e.imagePath,), opaque: false, transition: Transition.downToUp);
+              }
+              else{
+                FieldGuideElementAnimal element = Get.find<FieldGuidePageController>().animalElements.firstWhere((element) => element.id == e.registerInfo.dictionaryId);
+                Get.to(DetailAnimal(element: element, imagePath: e.imagePath,), opaque: false, transition: Transition.downToUp);
+              }
+            }
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children:[
+              Positioned(
+                child: Image.asset("assets/icons/marker.png", width: 49.w,),
+              ),
+              Positioned(
+                top: 2.w,
+                child: Container(
+                  width: 45.w,
+                  height: 45.w,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50.r),
+                    child: Image.network(
+                      e.imagePath,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+              ),
+            ]
+          ),
+        );
+      }).toList()),
+      builder: (BuildContext context, Set<Marker>? markers){
+        return GoogleMap(
+          zoomControlsEnabled: false,
+          onMapCreated: Get.find<HomePageController>().onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: Get.find<UserController>().curPosition.value,
+            zoom: 15,
+          ),
+          markers: markers ?? <Marker>{},
+          // markers: Set<Marker>.from(Get.find<HomePageController>().reports.map(
+          //   (e) => Marker(
+          //     markerId: MarkerId(e.reportId.toString()),
+          //     position: e.registerInfo.coor,
+          //     onTap: (){
+          //       bool ifPlant = false;
+          //       Get.find<FieldGuidePageController>().plantElements.forEach((element) {
+          //         if(element.id == e.registerInfo.dictionaryId){
+          //           ifPlant = true;
+          //         }
+          //       });
+          //
+          //       if(ifPlant){
+          //         FieldGuideElementPlant element = Get.find<FieldGuidePageController>().plantElements.firstWhere((element) => element.id == e.registerInfo.dictionaryId);
+          //         Get.to(DetailPlant(element: element, imagePath: e.imagePath,), opaque: false);
+          //       }
+          //       else{
+          //         FieldGuideElementAnimal element = Get.find<FieldGuidePageController>().animalElements.firstWhere((element) => element.id == e.registerInfo.dictionaryId);
+          //         Get.to(DetailAnimal(element: element, imagePath: e.imagePath,), opaque: false);
+          //       }
+          //     }
+          //   )
+          // ),)
+        );
+      }
     )),
   );
 }
