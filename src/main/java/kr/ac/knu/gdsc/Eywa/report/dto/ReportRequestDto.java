701a7d29e65dae5197cc685b13b84510ea9aa9ef
LEESEUNGYEOL <lsy12210@gmail.com>
2023-03-27 17:04:40 +0900

    fix: Annotation duplication

--- src/main/java/kr/ac/knu/gdsc/Eywa/report/dto/ReportRequestDto.java
+++ src/main/java/kr/ac/knu/gdsc/Eywa/report/dto/ReportRequestDto.java
@@ -13,8 +13,10 @@ public class ReportRequestDto {
     @DecimalMin("0.0000001")
     @Digits(integer = 10, fraction = 6)
     private BigDecimal longitude;
+
     @DecimalMin("0.0000001")
     @Digits(integer = 10, fraction = 6)
     private BigDecimal latitude;
+
     private Long dictionaryId;
 }
