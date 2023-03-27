30cc9282c60fdd1ee929677b71ec83cd9ad616ba
LEESEUNGYEOL <lsy12210@gmail.com>
2023-03-27 17:06:52 +0900

    fix: Annotation duplication error

--- src/main/java/kr/ac/knu/gdsc/Eywa/report/dto/ReportRequestDto.java
+++ src/main/java/kr/ac/knu/gdsc/Eywa/report/dto/ReportRequestDto.java
@@ -9,7 +9,6 @@ import java.math.BigDecimal;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReportRequestDto {
     @DecimalMin("0.0000001")
     @Digits(integer = 10, fraction = 6)
